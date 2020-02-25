66c467e49756789d011ed1b1a93c46ea7045e07d
ElDesalmado <sergey.kolesnik.ld@gmail.com>
2020-02-25 20:25:20 +0500

    Initial comit

---
+++ include/cmwComWrapper/com_wrapper.h
@@ -0,0 +1,602 @@
+﻿#pragma once
+
+#include <map>
+
+#include <type_traits>
+#include <variant>
+#include <cassert>
+
+#include <atomic>
+
+#include <combaseapi.h>
+#include <comdef.h>
+
+namespace cmw
+{
+    struct COMContext
+    {
+        COMContext(bool multithreaded = true);
+        ~COMContext();
+    };
+
+    template <typename T, 
+        class = std::enable_if_t<std::is_base_of_v<IUnknown, T>>>
+    class com_ptr
+    {
+        // dummy class, workaround. 
+        // Otherwise error due to not being able to inherit from final class 
+        template <class C, bool = std::is_final_v<C>>
+        struct hide_refs {};
+
+        // hide IUnknown methods if T is not final
+        template <class C>
+        struct hide_refs<C, false> : public C
+        {
+
+        protected:
+            using IUnknown::AddRef;
+            using IUnknown::Release;
+            using IUnknown::QueryInterface;
+
+            friend class com_ptr<T>;
+        };
+
+        using ptr_type = std::conditional_t<std::is_final_v<T>, T, hide_refs<T>>;
+
+        ptr_type *rawPtr_;
+
+    public:
+
+        com_ptr()
+            : rawPtr_(nullptr)
+        {}
+
+        com_ptr(const com_ptr& other)
+            : rawPtr_(other.rawPtr_)
+        {
+            if (IsValid())
+                rawPtr_->AddRef();
+        }
+
+        com_ptr(com_ptr&& other)
+            : rawPtr_(other.rawPtr_)
+        {
+            other.rawPtr_ = nullptr;
+        }
+
+        com_ptr& operator=(const com_ptr& other)
+        {
+            release_if_valid(rawPtr_);
+            rawPtr_ = other.rawPtr_;
+            if (IsValid())
+                rawPtr_->AddRef();
+            return *this;
+        }
+
+        com_ptr& operator=(com_ptr&& other)
+        {
+            release_if_valid(rawPtr_);
+            rawPtr_ = other.rawPtr_;
+            other.rawPtr_ = nullptr;
+
+            return *this;
+        }
+
+        // ownership must be transfered to this object
+        void Init(T* rawPtr)
+        {
+            rawPtr_ = static_cast<ptr_type*>(rawPtr);
+            assert(assert_ptr(rawPtr) && "Invalid raw pointer!");
+        }
+
+        // ownership must be transfered to this object
+        com_ptr(T* rawPtr)
+            : rawPtr_(static_cast<ptr_type*>(rawPtr))
+        {
+            assert(assert_ptr(rawPtr) && "Invalid raw pointer!");
+        }
+
+        bool IsValid() const
+        {
+            return (bool)rawPtr_;
+        }
+
+        operator bool() const
+        {
+            return IsValid();
+        }
+
+        template <typename Q, class = std::enable_if_t<std::is_base_of_v<IUnknown, Q>>>
+        com_ptr(Q *raw_parent)
+            : rawPtr_(nullptr)
+        {
+            HRESULT hr = raw_parent->QueryInterface((T**)&rawPtr_);
+            assert(SUCCEEDED(hr) && "Failed to query interface!");
+
+            if (!SUCCEEDED(hr))
+                throw _com_error(hr);
+        }
+
+        // TODO: return hresut code, do not throw
+        template <typename Q, class = std::enable_if_t<std::is_base_of_v<IUnknown, Q>>>
+        std::variant<com_ptr<Q>, HRESULT> QueryInterface()
+        {
+            Q *rawOut = nullptr;
+            HRESULT hr = rawPtr_->QueryInterface(__uuidof(Q), (void**)&rawOut);
+
+            if (!SUCCEEDED(hr))
+                return hr;
+                //throw _com_error(hr);
+
+            return com_ptr<Q>(rawOut);
+        }
+
+        template <typename Q, class = std::enable_if_t<std::is_base_of_v<IUnknown, Q>>>
+        operator com_ptr<Q>()
+        {
+            std::variant<com_ptr<Q>, HRESULT> ret = QueryInterface<Q>();
+            assert(std::holds_alternative<com_ptr<Q>>(ret) && "Failed to query interface!");
+            if (std::holds_alternative<HRESULT>(ret))
+                throw _com_error(std::get<1>(ret));
+
+            return std::move(std::get<0>(ret));
+        }
+
+        ptr_type* operator->()
+        {
+            assert(IsValid() && "Accessing nullptr!");
+            return rawPtr_;
+        }
+
+        const ptr_type* operator->() const
+        {
+            assert(IsValid() && "Accessing nullptr!");
+            return rawPtr_;
+        }
+
+        ptr_type& operator*()
+        {
+            assert(IsValid() && "Dereferencing nullptr!");
+            return *rawPtr_;
+        }
+
+        const ptr_type& operator*() const
+        {
+            assert(IsValid() && "Dereferencing nullptr!");
+            return *rawPtr_;
+        }
+
+        T* GetRaw()
+        {
+            return rawPtr_;
+        }
+
+        const T* GetRaw() const
+        {
+            return rawPtr_;
+        }
+
+        size_t RefsCount() const
+        {
+            if (!IsValid())
+                return 0;
+
+            ULONG refs = rawPtr_->AddRef();
+            refs = rawPtr_->Release();
+
+            return (size_t)refs;
+        }
+
+        ~com_ptr()
+        {
+            if (rawPtr_)
+                rawPtr_->Release();
+        }
+
+    private:
+        
+        static bool assert_ptr(T *ptr)
+        {
+            if (!ptr)
+                return false;
+            return true;
+        }
+
+        static void release_if_valid(T *ptr)
+        {
+            if (ptr)
+                ptr->Release();
+        }
+    };
+
+    template <class Interface>
+    struct transfer_com_ptr
+    {
+#undef interface
+
+        using v_interface = std::variant<com_ptr<Interface>, HRESULT>;
+        using ptr_com = com_ptr<Interface>;
+
+        v_interface temp_;
+
+        transfer_com_ptr(v_interface&& temp)
+            : temp_(std::move(temp))
+        {}
+
+        operator v_interface()
+        {
+            return std::move(temp_);
+        }
+
+        operator ptr_com()
+        {
+            assert(!std::holds_alternative<HRESULT>(temp_) &&
+                "Invalid Connection point received!");
+            if (std::holds_alternative<HRESULT>(temp_))
+                throw _com_error(std::get<HRESULT>(temp_));
+
+            return std::move(std::get<ptr_com>(temp_));
+        }
+    };
+
+    template <class Interface, class CoClass>
+    class CreateInstance : protected transfer_com_ptr<Interface>
+    {
+        using transfer = transfer_com_ptr<Interface>;
+
+    public:
+        static std::variant<com_ptr<Interface>, HRESULT> Create(tagCLSCTX clsContext,
+            IUnknown * pAggregate = nullptr)
+        {
+            Interface *pRes = nullptr;
+            HRESULT hr = CoCreateInstance(__uuidof(CoClass), pAggregate, clsContext,
+                __uuidof(Interface), (void**)&pRes);
+            if (!SUCCEEDED(hr))
+                return hr;
+
+            assert(pRes && "Interface is nullptr!");
+            return com_ptr<Interface>(pRes);
+        }
+
+        CreateInstance(tagCLSCTX clsContext,
+            IUnknown * pAggregate = nullptr) noexcept
+            : transfer(Create(clsContext, pAggregate))
+        {}
+
+        using transfer::operator transfer::v_interface;
+        using transfer::operator transfer::ptr_com;
+
+    };
+
+
+    template <class Interface = void, bool = std::is_void_v<Interface>>
+    class FindConnectionPoint : protected transfer_com_ptr<IConnectionPoint>
+    {
+        using transfer = transfer_com_ptr<IConnectionPoint>;
+
+    public:
+
+        static std::variant<com_ptr<IConnectionPoint>, HRESULT>
+            Find(IConnectionPointContainer& cpContainer, REFIID riid);
+
+        FindConnectionPoint(IConnectionPointContainer& cpContainer, REFIID riid) noexcept
+            : transfer(Find(cpContainer, riid))
+        {}
+
+        using transfer::operator transfer::v_interface;
+        using transfer::operator transfer::ptr_com;
+
+    };
+
+    template <class Interface>
+    class FindConnectionPoint<Interface, false> : public FindConnectionPoint<void>
+    {
+        static_assert(std::is_base_of_v<IUnknown, Interface>,
+            "Invalid interface! Must inherit from IUnknown!");
+
+        static_assert(!(std::is_same_v<IUnknown, Interface> ||
+            std::is_same_v<IDispatch, Interface> ||
+            std::is_same_v<IConnectionPoint, Interface>), "Class must have a valid unique REFIID");
+   
+        using base = FindConnectionPoint<void>;
+    public:
+
+        static std::variant<com_ptr<IConnectionPoint>, HRESULT>
+            Find(IConnectionPointContainer& cpContainer)
+        {
+            // TODO: assert __uuidof is not null
+            return FindConnectionPoint(cpContainer, __uuidof(Interface));
+        }
+
+        FindConnectionPoint(IConnectionPointContainer& cpContainer) noexcept
+            : base(cpContainer, __uuidof(Interface))
+        {}
+
+    };
+    
+    // this class implements multiple connections for a com object. 
+    // Connectible object may inherit from it to provide RegConnectionPoint 
+    class com_connections
+    {
+        // pointers to IConnectionPoints must be 'Alive' by the moment Unadvise is called 
+        std::map<DWORD, com_ptr<IConnectionPoint>> connections_;
+
+    public:
+
+        size_t NumConnections() const
+        {
+            return connections_.size();
+        }
+
+        void RegConnectionPoint(DWORD cookie, com_ptr<IConnectionPoint>& cpoint)
+        {
+            connections_.emplace(cookie, cpoint);
+        }
+
+        std::variant<HRESULT, bool> Disconnect(DWORD cookie)
+        {
+            auto found = connections_.find(cookie);
+            if (found == connections_.end())
+                return false;
+
+            com_ptr<IConnectionPoint>& cp = found->second;
+            HRESULT hr = cp->Unadvise(found->first);
+
+            connections_.erase(found);
+
+            return hr;
+        }
+
+        // 
+        HRESULT DisconnectAll()
+        {
+            HRESULT res = S_OK;
+
+            using iter = decltype(connections_)::iterator;
+
+            iter it = connections_.begin();
+            while (it != connections_.end())
+            {
+                com_ptr<IConnectionPoint>& cp = it->second;
+                HRESULT hr = cp->Unadvise(it->first);
+                if (!SUCCEEDED(hr))
+                    res = hr;
+                ++it;
+            }
+
+            connections_.clear();
+            return res;
+        }
+
+        com_connections() = default;
+        ~com_connections()
+        {
+            HRESULT hr = DisconnectAll();
+            assert(SUCCEEDED(hr));
+        }
+
+    };
+
+    template <class Dispatch>
+    class listener_traits
+    {
+        using t_reg_cpoint = void(Dispatch::*)(DWORD, IConnectionPoint&);
+        using t_disconnect = std::variant<HRESULT, bool>(Dispatch::*)(DWORD);
+
+        template <class T, class = std::void_t<>>
+        struct has_reg_cpoint : std::false_type {};
+
+        template<class T>
+        struct has_reg_cpoint<T,
+            std::void_t<decltype(t_reg_cpoint(&T::RegConnectionPoint))>> :
+            std::true_type {};
+
+        template <class T, class = std::void_t<>>
+        struct has_disconnect : std::false_type {};
+
+        template<class T>
+        struct has_disconnect<T,
+            std::void_t<decltype(t_reg_cpoint(&T::Disconnect))>> :
+            std::true_type {};
+
+    public:
+
+        using type = Dispatch;
+        constexpr static bool inherit_idispatch = std::is_base_of_v<IDispatch, Dispatch>;
+        constexpr static bool has_reg_cpoint_v = has_reg_cpoint<Dispatch>();
+        constexpr static bool has_disconnect_v = has_disconnect<Dispatch>();
+
+        constexpr static bool is_connectible = inherit_idispatch &&
+            has_reg_cpoint_v &&
+            has_disconnect_v;
+
+    };
+
+    template <class T>
+    struct tag_iid {};
+
+    template <class Connectible,
+        class = std::enable_if_t<listener_traits<Connectible>::is_connectible>>
+        class ConnectListener
+    {
+        HRESULT hr_ = S_OK;
+
+    public:
+
+        // does not register dword cookie in Connectible's connections map
+        static std::variant<DWORD, HRESULT> Connect(com_ptr<IUnknown>& pSink,
+            IConnectionPoint& cpoint)
+        {
+            DWORD cookie = 0;
+            HRESULT hr = cpoint.Advise(pSink.GetRaw(), &cookie);
+            if (!SUCCEEDED(hr))
+                return hr;
+            return cookie;
+        }
+
+        static HRESULT Connect(com_ptr<Connectible>& connectible,
+            com_ptr<IConnectionPoint>& cpoint)
+        {
+            com_ptr<IUnknown> pSink = connectible;
+            std::variant<DWORD, HRESULT> vCookie = Connect(pSink, *cpoint);
+            if (std::holds_alternative<HRESULT>(vCookie))
+                return std::get<HRESULT>(vCookie);
+
+            connectible->RegConnectionPoint(std::get<DWORD>(vCookie), cpoint);
+
+            return S_OK;
+        }
+
+        static std::variant<HRESULT, bool> Disconnect(com_ptr<Connectible>& connectible, 
+            DWORD cookie)
+        {
+            return connectible->Disconnect(cookie);
+        }
+
+        ConnectListener(com_ptr<Connectible>& connectible,
+            com_ptr<IConnectionPoint>& cpoint)
+            : hr_(Connect(connectible, cpoint))
+        {}
+
+        // something is wrong. If this method is used, UnAdvise returns "Object is not connected to server"
+        template <class Interface, class Provider>
+        ConnectListener(com_ptr<Connectible>& connectible, com_ptr<Provider>& cpProvider, 
+            tag_iid<Interface>)
+        {
+            
+            try
+            {
+                com_ptr<IConnectionPointContainer> cpContainter = cpProvider;
+                com_ptr<IConnectionPoint> cPoint =
+                    FindConnectionPoint<Interface>(*cpContainter);
+                hr_ = Connect(connectible, cPoint);
+            }
+            catch (const _com_error& error)
+            {
+                hr_ = error.Error();
+            }
+           
+        }
+
+        operator HRESULT() const
+        {
+            return hr_;
+        }
+
+    };
+
+
+    template <class Interface, class CoClass, class Dispatch>
+    class com_obj
+    {
+    protected:
+
+        com_ptr<Interface> pInterface_;
+        com_ptr<Dispatch> pDispInterface_;
+
+    public:
+
+        using coclass = CoClass;
+        using interface = Interface;
+        using dispinterface = Dispatch;
+
+        com_obj() = default;
+
+        com_obj(const com_ptr<Interface>& pInterface)
+            : pInterface_(pInerface),
+            pDispInterface_(pInterface_)
+        {}
+
+        operator const com_ptr<Interface>&()
+        {
+            return pInterface_;
+        }
+        operator const com_ptr<Dispatch>&()
+        {
+            return pDispInterface_;
+        }
+
+        HRESULT CreateInstance(tagCLSCTX clsContext, IUnknown *pAggregate = nullptr)
+        {
+            std::variant<com_ptr<Interface>, HRESULT> vInterface =
+                cmw::CreateInstance<Interface, CoClass>(clsContext, pAggregate);
+            
+            if (std::holds_alternative<HRESULT>(vInterface))
+                return std::get<HRESULT>(vInterface);
+
+            pInterface_ = std::move(std::get<0>(vInterface));
+            return S_OK;
+        }
+
+        com_obj(tagCLSCTX clsContext,
+            IUnknown * pAggregate = nullptr)
+        {
+            try
+            {
+                pInterface_ = CreateInstance<Interface, CoClass>(clsContext, pAggregate);
+                pDispInterface_ = pInterface_;
+            }
+            catch (const _com_error& error)
+                throw error;
+        }
+        
+    private:
+
+        static void check_dispatch()
+        {
+            if constexpr (std::is_same_v<void, Dispatch>)
+                return;
+            else
+                static_assert(std::is_base_of_v<IDispatch, Dispatch>,
+                    "Dispinterface must inherit from IDispatch!");
+        }
+    };
+
+    // helper class. Implements thread safe reference counting
+    class reference_counter
+    {
+        std::atomic<ULONG> refs_ = 1;
+
+    public:
+        ULONG AddRef()
+        {
+            ++refs_;
+            return refs_;
+        }
+
+        ULONG Release()
+        {
+            --refs_;
+#undef max
+            assert((refs_ != std::numeric_limits<ULONG>::max()) && "Invalid value!");
+            return refs_;
+        }
+
+        ULONG RefsCount() const
+        {
+            return refs_;
+        }
+
+        constexpr reference_counter() = default;
+        ~reference_counter() = default;
+
+    };
+
+
+
+
+    /*
+    template <class COM, class Interface, class Disp>
+    struct com_traits
+    {
+        static_assert(std::is_base_of_v<IDispatch, Disp>, "Invalid Dispathc Interface");
+        static_assert(std::is_base_of_v<IUnknown, Interface>, "Type must inherit from IUnknown");
+
+        using coclass = COM;
+        using interface = Interface;
+        using dispinterface = Disp;
+
+    };
+    */
+    
+}
---
+++ src/com_wrapper.cpp
@@ -0,0 +1,36 @@
+﻿#include "com_wrapper.h"
+
+#include <exception>
+
+
+using namespace cmw;
+
+COMContext::COMContext(bool multithreaded)
+{
+    tagCOINIT threads = multithreaded ? 
+        COINIT_MULTITHREADED :
+        COINIT_APARTMENTTHREADED;
+
+    HRESULT res = CoInitializeEx(0, threads);
+
+    assert(SUCCEEDED(res) && "Failed to initialize COM");
+
+    if (!SUCCEEDED(res))
+        throw std::exception("Failed to initialize COM");
+}
+
+COMContext::~COMContext()
+{
+    CoUninitialize();
+}
+
+
+std::variant<com_ptr<IConnectionPoint>, HRESULT> FindConnectionPoint<void>::Find(IConnectionPointContainer & cpContainer, REFIID riid)
+{
+    IConnectionPoint *pCp = nullptr;
+    HRESULT hr = cpContainer.FindConnectionPoint(riid, &pCp);
+    if (!SUCCEEDED(hr))
+        return hr;
+
+    return com_ptr<IConnectionPoint>(pCp);
+}
